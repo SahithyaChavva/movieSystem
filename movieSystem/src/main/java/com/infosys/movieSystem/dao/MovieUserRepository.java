1d1de89cd9e90f902baa0e7be479a7b3f6362a72
SahithyaChavva <chavvasahithyareddy@gmail.com>
2024-11-23 23:36:10 +0530

    Delete movieSystem directory

--- movieSystem/src/main/java/com/infosys/movieSystem/dao/MovieUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.movieSystem.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.movieSystem.bean.MovieUser;
-
-public interface MovieUserRepository extends JpaRepository<MovieUser, String> {
-	
-}
